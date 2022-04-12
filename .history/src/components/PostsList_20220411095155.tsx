74944c974248c22c3e0deb0eee579d7c77555772
barridev <j_barri@etu-webschoolfactory.fr>
2022-04-12 09:56:18 +0200

    Initial commit

---
+++ .history/src/components/PostsList_20220411095155.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Post } from '../api/types'
+import PostItem from './PostItem'
+
+const PostsList = () => {
+    const [posts, setPosts] = useState<Array<Post>>([])
+    const [loading, setLoading] = useState(false)
+
+    function renderItem(values: Post) {
+        return (
+            <div key={values.id}>
+                <PostItem {...values} />
+            </div>
+        )
+    }
+
+    if (loading) {
+        return (
+            <section className="hero">
+                <div className="hero-body">
+                    <p className="title">Loading ...</p>
+                </div>
+            </section>
+        )
+    }
+
+    if (posts.length === 0) {
+        return (
+            <section className="hero">
+                <div className="hero-body">
+                    <p className="title">No Posts</p>
+                </div>
+                <br></br>
+                <p>Ceci est un post</p>
+                <p>YO</p>
+                <p>le contenu du post</p>
+                <p>easy en fait</p>
+            </section>
+        )
+    }
+
+    return <ul className="post-list">{posts.map(renderItem)}</ul>
+}
+
+export default PostsList
